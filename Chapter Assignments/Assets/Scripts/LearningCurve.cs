b7758b300463fe8a66028eb1cac2a05ad8684ccf
Axel Carmona-Medina <carmonamedina@colum.edu>
2025-09-09 18:25:00 -0500

    Chapter 4 Added

--- Chapter Assignments/Assets/Scripts/LearningCurve.cs
+++ Chapter Assignments/Assets/Scripts/LearningCurve.cs
@@ -9,14 +9,41 @@ public class LearningCurve : MonoBehaviour
     public float Pi = 3.14f; 
     public string FirstName = "Harrison"; 
     public bool IsAuthor = true;
+    public bool HasDungeonKey = true;
+    public int CurrentGold = 32;
+    public bool WeaponEquipped = true; 
+    public string WeaponType = "Longsword";
+    public bool PureOfHeart = true; 
+    public bool HasSecretIncantation = false; 
+    public string RareItem = "Relic Stone";
 
     // Start is called before the first frame update
     void Start()
     {
+        Thievery();
+        ComputeAge();
+        OpenTreasureChamber();
         int characterLevel = 32; 
         int nextSkillLevel = GenerateCharacter("Spike", characterLevel); 
         Debug.Log(nextSkillLevel); 
         Debug.Log(GenerateCharacter("Faye", characterLevel));
+        if (HasDungeonKey) { Debug.Log("You possess the sacred key: enter."); 
+        } 
+        else 
+        { 
+         Debug.Log("You have not proved yourself yet."); 
+        }
+        if (WeaponEquipped) 
+        { 
+          if (WeaponType == "Longsword") 
+          { 
+            Debug.Log("For the Queen!"); 
+          } 
+        } 
+        else 
+        { 
+          Debug.Log("Fists aren't going to work against armor..."); 
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +56,24 @@ public class LearningCurve : MonoBehaviour
     /// Time for action - adding comments     
     /// Computes a modified age integer      
     ///</summary>   
-    
+    public void OpenTreasureChamber()
+    {
+        if (PureOfHeart && RareItem == "Relic Stone")
+        {
+            if (!HasSecretIncantation)
+            {
+                Debug.Log("You have the spirit, but not the knowledge.");
+            }
+            else
+            {
+                Debug.Log("The treasure is yours, worthy hero!");
+            }
+        }
+        else
+        {
+            Debug.Log("Come back when you have what it takes.");
+        }
+    }
     void ComputeAge()     
     {         
         Debug.Log(CurrentAge + AddedAge);     
@@ -39,5 +83,20 @@ public class LearningCurve : MonoBehaviour
     {
         //Debug.LogFormat("Character: {0} - Level: {1}", name, level);
         return level += 5;
+    }
+
+    public void Thievery() 
+    { if (CurrentGold > 50) 
+      { 
+       Debug.Log("You're rolling in it!"); 
+      } 
+      else if (CurrentGold < 15) 
+      { 
+        Debug.Log("Not much there to steal...");
+      } 
+      else 
+      { 
+       Debug.Log("Looks like your purse is in the sweet spot.");
+      }
     }
 }
