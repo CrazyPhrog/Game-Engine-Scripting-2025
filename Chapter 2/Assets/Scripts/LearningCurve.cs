29a3b82197fa9f64049f65434a4f47249b722bde
Axel Carmona-Medina <carmonamedina@colum.edu>
2025-09-08 21:32:30 -0500

    Chapter 2
    This is the work done in Chapter 2.

---
+++ Chapter 2/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int CurrentAge = 30;
+    public int AddedAge = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComputeAge();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ComputeAge()
+    {
+        Debug.Log(CurrentAge + AddedAge);
+    }
+}
