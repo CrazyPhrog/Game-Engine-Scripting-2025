90b7d4a1262c69cdc19a7269d60361c58270635a
Axel Carmona-Medina <carmonamedina@colum.edu>
2025-09-30 00:05:52 -0500

    Chapter 67

---
+++ Chapter Assignments2/Assets/Script/ItemBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.gameObject);
+            Debug.Log("Item collected!");
+        }
+    }
+        
+   
+    
+    
+}
+
---
+++ Chapter Assignments2/Assets/Script/PlayerBehavior.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float RotateSpeed = 75f;
+    public float MoveSpeed = 10f;
+
+    private float _vInput;
+    private float _hInput;
+
+    private Rigidbody _rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * _hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _vInput = Input.GetAxis("Vertical") * MoveSpeed;
+        _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+        //
+        //this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
+        // this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+        //
+    }
+}
